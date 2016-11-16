402b0d7481a2c2adefb1026118e5a7b5aeba6ec0
Andrew Woods <atwoods1@gmail.com>
2016-11-16 14:37:32 -0800

    Add file size constants
    Constants for kilobytes, megabytes, gigabytes, terabytes, and petabytes.

---
+++ src/constants/filesize-constants.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * File Size Constants
+ *
+ * @package Core
+ */
+
+
+/**
+ * Kilobytes
+ */
+define('KB_IN_BYTES', 1024);
+define('KILOBYTES', KB_IN_BYTES);
+
+/**
+ * Megabytes
+ *
+ * 1MB = 1,048,576 Bytes
+ */
+define('MB_IN_BYTES', 1024 * KB_IN_BYTES);
+define('MEGABYTES', MB_IN_BYTES);
+
+/**
+ * Gigabytes
+ *
+ * 1GB = 1,073,741,824 Bytes
+ */
+define('GB_IN_BYTES', 1024 * MB_IN_BYTES);
+define('GIGABYTES', GB_IN_BYTES);
+
+/**
+ * Terabytes
+ *
+ * 1 TB = 1,099,511,627,776
+ */
+define('TB_IN_BYTES', 1024 * GB_IN_BYTES);
+define('TERABYTES', TB_IN_BYTES);
+
+/**
+ * Petabytes
+ *
+ * 1 PB = 1,125,899,906,842,624 Bytes
+ */
+define('PB_IN_BYTES', 1024 * TB_IN_BYTES);
+define('PETABYTES', PB_IN_BYTES);
+
+
