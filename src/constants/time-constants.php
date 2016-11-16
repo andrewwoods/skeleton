560965de4a98a663a0320b11bffea738c21e6afb
Andrew Woods <atwoods1@gmail.com>
2016-11-16 14:43:30 -0800

    Add time related constants
    Different units of time measured in seconds.

---
+++ src/constants/time-constants.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Time Constants
+ *
+ * @package Core
+ */
+
+
+/**
+ * The number of seconds for 1 minute
+ */
+define('MINUTE_IN_SECONDS', 60);
+
+/**
+ * The number of seconds for 1 hour
+ */
+define('HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS);
+
+/**
+ * The number of seconds for 1 day
+ */
+define('DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS);
+
+/**
+ * The number of seconds for 1 week (7 days)
+ */
+define('WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS);
+
+/**
+ * The number of seconds for 1 month (30 days)
+ */
+define('MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS);
+
+/**
+ * The number of seconds for 1 year (365 days)
+ */
+define('YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS);
+
+
