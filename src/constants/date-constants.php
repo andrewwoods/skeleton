e673cf2a5d49b560509f3640ce9129a33c9488f9
Andrew Woods <atwoods1@gmail.com>
2016-11-16 14:35:00 -0800

    Add date constants
    Date format constants - ISO and US Date formats.

---
+++ src/constants/date-constants.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Date Constants
+ *
+ * @package Core
+ */
+
+
+/**
+ * Short ISO date - in the format of YYYY-MM-DD
+ *
+ * e.g. 2016-11-03 for November 3, 2016
+ */
+define('DATE_ISO_FORMAT', 'Y-m-d');
+
+/**
+ * Long ISO date - in the format of YYYY-MM-DD HH:MM:SS
+ *
+ * e.g. 2016-11-03 23:36:49 for November 3, 2016 11:36:49 PM
+ */
+define('DATE_ISO_LONG_FORMAT', 'Y-m-d G:i:s');
+
+/**
+ * Short USA-style date - in the format of MM/DD/YYYY
+ *
+ * e.g. 11/03/2016 for November 3, 2016
+ */
+define('DATE_US_FORMAT', 'm/d/Y');
+
+/**
+ * Medium USA-style date - in the format of Month, Date Year
+ *
+ * e.g. November 3, 2016 for 2016-11-03
+ */
+define('DATE_US_MEDIUM_FORMAT', 'F j, Y');
+
+/**
+ * Long USA-style date - Weekday, Month Date, Year
+ *
+ * e.g. for November 3, 2016 for 2016-11-03
+ */
+define('DATE_US_LONG_FORMAT', 'l, F jS, Y');
+
+
+
