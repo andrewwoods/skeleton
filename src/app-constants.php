20d696b0936c97bfe20005a9326c2a69c60c9c3e
Andrew Woods <atwoods1@gmail.com>
2016-11-16 14:33:47 -0800

    Add application constants
    Add generic version and directory constants.

---
+++ src/app-constants.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Your Application Constants
+ *
+ * @package Core
+ */
+
+
+/*
+ * Version Constants
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * The application uses semantic versioning
+ *
+ * @link http://semver.org Semantic Versioning website
+ */
+define('VERSION', '0.1.0');
+
+/**
+ * The minimum version of PHP needed
+ *
+ * @link https://secure.php.net/releases/5_2_2.php
+ *
+ */
+define('MINIMUM_PHP_VERSION', '7.0.0');
+
+
+/*
+ * Application Directory Constants
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * This represents the root of the project
+ *
+ * It's where each of the applications are located
+ */
+define('APP_DIR', dirname(__FILE__));
+
+/**
+ * The 'etc' directory is a directory for things that don't fit elsewhere
+ *
+ */
+define('ETC_DIR', APP_DIR . '/etc');
+
+/**
+ * The 'lib' directory is a library for your application
+ *
+ */
+define('LIB_DIR', APP_DIR . '/lib');
+
+/**
+ * Global include path
+ */
+define('PUBLIC_DIR', APP_DIR . '/public');
+
+
