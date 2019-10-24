6787e838d7b314c738057fed4fbd1e15901262d6
Andrew Woods <atwoods1@gmail.com>
2019-10-23 23:00:27 -0400

    Remove license option from Docs command
    It's not currently being used.

--- src/Commands/CreateDocsCommand.php
+++ src/Commands/CreateDocsCommand.php
@@ -5,7 +5,6 @@ namespace Skel\Commands;
 use Skel\Document;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -14,8 +13,6 @@ class CreateDocsCommand extends Command
     protected static $defaultName = 'docs';
 
     protected static $defaultDoc = 'all';
-
-    protected static $defaultLicense = 'mit';
 
 
     /**
@@ -58,9 +55,6 @@ class CreateDocsCommand extends Command
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $opt = [];
-        $opt['licenseType'] = $input->getOption('license') ?? false;
-
         $arg = [];
         $arg['names'] = $input->getArgument('names') ?? self::$defaultDoc;
         if ( count($arg['names']) === 0 || $arg['names'][0] === self::$defaultDoc ) {
@@ -69,9 +63,6 @@ class CreateDocsCommand extends Command
 
         $document = new Document();
         foreach ($arg['names'] as $doc) {
-            if ($doc == "license" && isset($opt['licenseType'])) {
-                $doc .= '-' . $this->sanitizeLicenseName($opt['licenseType']);
-            }
 
             $pathSource = $this->skeletonPath . '/'
                 . $document->getSourceFileName($doc);
@@ -83,13 +74,6 @@ class CreateDocsCommand extends Command
 
     }
 
-    protected function sanitizeLicenseName($license)
-    {
-        $license = strtolower($license);
-
-        return $license;
-    }
-
     protected function configure()
     {
         $helpNames = 'specify the file name. choose from: ' .
@@ -99,13 +83,6 @@ class CreateDocsCommand extends Command
         $this
             ->setDescription('Creates project docs.')
             ->setHelp('Generate boilerplate documentation for your project')
-            ->addOption(
-                'license',
-                'l',
-                InputOption::VALUE_REQUIRED,
-                'type of software license',
-                self::$defaultLicense
-            )
             ->addArgument(
                 'names',
                 InputArgument::IS_ARRAY | InputArgument::OPTIONAL,
